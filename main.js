ceffabbe778e0aa43418fd58f31820a757dbd8e6
AlexandraAndrei99 <109028019+AlexandraAndrei99@users.noreply.github.com>
2022-07-21 22:05:20 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -14,4 +14,17 @@ const butonReq = document.querySelector('.heroSection button')
 butonReq.addEventListener("click",() => {
     colorH1.style.color= "pink";
 })
+const hamburger= document.querySelector('.hamburger')
+const meniu = document.querySelector('.menu')
+const outNav = document.querySelector('.outNav')
+
+hamburger.addEventListener("click", () =>{
+    meniu.classList.toggle("menuToggler")
+    outNav.classList.toggle("outNavToggler")
+    hamburger.classList.toggle("hamburgerToggler")
+    
+} )
+
+
+
 
